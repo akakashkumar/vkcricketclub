20cb36f66335155d666a90e2e2a9cd6b5a9c1ccc
Akash Kumar <akashkumarmahto2006@gmail.com>
2025-04-20 11:35:46 +0530

    update images

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react'
 import LocomotiveScroll from 'locomotive-scroll';
 
 const page = () => {
-     const [sliderPics, setSliderPics] = useState(['images/banner.jpg', 'images/banner.jpg', 'images/banner.jpg', 'images/banner.jpg'])
+     const [sliderPics, setSliderPics] = useState(['Images/banner.jpg', 'Images/banner.jpg', 'Images/banner.jpg', 'Images/banner.jpg'])
     const [pics, setPics] = useState([
         'Images/banner.jpg',
         'Images/banner.jpg',
